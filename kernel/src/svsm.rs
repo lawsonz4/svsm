1dc9d1ea29438b91caa9d64970c4f666e615d0ce
Stefano Garzarella <sgarzare@redhat.com>
2024-07-31 17:23:58 +0200

    Add BlockFS support

--- kernel/src/svsm.rs
+++ kernel/src/svsm.rs
@@ -33,6 +33,7 @@ use svsm::cpu::sse::sse_init;
 use svsm::debug::gdbstub::svsm_gdbstub::{debug_break, gdbstub_start};
 use svsm::debug::stacktrace::print_stack;
 use svsm::enable_shadow_stacks;
+use svsm::fs::initialize_blk;
 use svsm::fs::{initialize_fs, opendir, populate_ram_fs};
 use svsm::hyperv::hyperv_setup;
 use svsm::igvm_params::IgvmParams;
@@ -361,6 +362,8 @@ pub extern "C" fn svsm_main(cpu_index: usize) {
         // Nothing to do with data at the moment, simply print a success message.
         log::info!("attestation successful");
     }
+
+    initialize_blk();
 
     #[cfg(all(feature = "vtpm", not(test)))]
     vtpm_init().expect("vTPM failed to initialize");
