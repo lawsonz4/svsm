14c62d68c621453d17810d1f4c031335704f0c0a
Oliver Steffen <osteffen@redhat.com>
2025-08-13 10:54:56 +0200

    fixup! scripts/launch_guest.sh: use 2G to dump memory quickly

--- scripts/launch_guest.sh
+++ scripts/launch_guest.sh
@@ -135,7 +135,7 @@ if (( QEMU_MAJOR >= 9 )); then
   IGVM_OBJ="-object igvm-cfg,id=igvm0,file=$IGVM"
 elif (( (QEMU_MAJOR > 8) || ((QEMU_MAJOR == 8) && (QEMU_MINOR >= 2)) )); then
   MACHINE=q35,confidential-guest-support=sev0,memory-backend=mem0,accel=$ACCEL
-  MEMORY=memory-backend-memfd,size=8G,id=mem0,share=true,prealloc=false,reserve=false
+  MEMORY=memory-backend-memfd,size=2G,id=mem0,share=true,prealloc=false,reserve=false
   SNP_GUEST="-object sev-snp-guest,id=sev0,cbitpos=$C_BIT_POS,reduced-phys-bits=1,init-flags=5,igvm-file=$IGVM"
 else
   MACHINE=q35,confidential-guest-support=sev0,memory-backend=mem0,kvm-type=protected,accel=$ACCEL
